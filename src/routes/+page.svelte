3461e3acb1f9d1afed351bb8ebf4027206a84d94
avahuu <xmhu312@gmail.com>
2026-04-08 20:39:29 -0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import rawBlocks from '$lib/doc.blocks.json';
 	import DocRenderer from '$lib/components/DocRenderer.svelte';
-	import TimelineWaterfall from '$lib/components/TimelineWaterfall.svelte';
-	import HeatmapComparison from '$lib/components/HeatmapComparison.svelte';
 	import type { Block } from '$lib/components/DocRenderer.svelte';
 	const blocks = rawBlocks as Block[];
 </script>
-<TimelineWaterfall />
-<HeatmapComparison />
 
 <!-- DO NOT EDIT (except to modify classes). This will become code imported from Google Doc. -->
-<div class="container">
-	<div class="row justify-content-center">
-		<div class="col-12 col-sm-10 col-lg-8 col-xxl-6">
-			<DocRenderer {blocks} />
-		</div>
-	</div>
+<div>
+	<DocRenderer {blocks} />
 </div>
 
 <!-- Hard-code any custom code that should appear AFTER Google Doc below here. -->
